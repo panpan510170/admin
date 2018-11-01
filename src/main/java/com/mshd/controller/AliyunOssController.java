8199a4b7031cb495067de4461b980aeea03a42d8
pangaofeng <pangaofeng>
2018-11-01 14:38:16 +0800

    OSS STS  获取token

---
+++ src/main/java/com/mshd/controller/AliyunOssController.java
@@ -0,0 +1,118 @@
+package com.mshd.controller;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.sts.model.v20150401.AssumeRoleResponse;
+import com.mshd.enums.ResultCodeEnum;
+import com.mshd.model.User;
+import com.mshd.util.OSSFileUpLoadUtils;
+import com.mshd.vo.JsonResult;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.sql.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Api(tags = {"阿里云OSS"})
+@RestController
+@RequestMapping(value = {"/aliyunOss"})
+public class AliyunOssController extends BaseController{
+
+    @Value(value = "${aliyun.oss.endpoint}")
+    private String endpoint;
+    @Value(value = "${aliyun.oss.AccessKeyID}")
+    private String AccessKeyID;
+    @Value(value = "${aliyun.oss.AccessKeySecret}")
+    private String AccessKeySecret;
+
+
+    @Value(value = "${aliyun.oss.sonAccessKeyID}")
+    private String sonAccessKeyID;
+    @Value(value = "${aliyun.oss.sonAccessKeySecret}")
+    private String sonAccessKeySecret;
+    @Value(value = "${aliyun.oss.roleArn}")
+    private String roleArn;
+
+    @GetMapping(value = "/getSign")
+    public JsonResult getSign(HttpServletRequest request) throws UnsupportedEncodingException {
+
+        String bucket = "midautumn";
+        if ("1".equals(request.getHeader("fromId")))
+            bucket = "midautumn";
+        else if ("2".equals(request.getHeader("fromId")))
+            bucket = "midautumn1";
+        String dir = "files/";
+        String host = "https://" + bucket + "." + endpoint;
+        System.out.println(host);
+        OSSClient client = new OSSClient(endpoint, AccessKeyID, AccessKeySecret);
+        long expireTime = 30;
+        long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+        Date expiration = new Date(expireEndTime);
+        PolicyConditions policyConds = new PolicyConditions();
+        policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+        policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+        String postPolicy = client.generatePostPolicy(expiration, policyConds);
+        byte[] binaryData = postPolicy.getBytes("utf-8");
+        String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+        String postSignature = client.calculatePostSignature(postPolicy);
+
+        Map<String, String> respMap = new LinkedHashMap<String, String>();
+        respMap.put("accessid", AccessKeyID);
+        respMap.put("policy", encodedPolicy);
+        respMap.put("signature", postSignature);
+        //respMap.put("expire", formatISO8601Date(expiration));
+        respMap.put("dir", dir);
+        respMap.put("host", host);
+        respMap.put("expire", String.valueOf(expireEndTime / 1000));
+        return this.buildSuccessResult(respMap);
+    }
+
+    /**
+     * 需要在RAM控制台获取，此时要给子账号权限，并建立一个角色，把这个角色赋给子账户，这个角色会有一串值，就是rolearn要填的　　　　　　　
+     * 记得角色的权限，子账户的权限要分配好，不然会报错
+     *
+     * 过期时间默认一小时
+     * */
+
+    @GetMapping(value = "/getToken")
+    public JsonResult getToken(HttpServletRequest request) throws Exception {
+
+        User user = (User) request.getSession().getAttribute("user");
+
+        if (null == user) {
+            return this.buildErrorResult(ResultCodeEnum.paramError.getId(), "用户信息未获取到,请重新登录");
+        }
+
+        //临时Token的会话名称，自己指定用于标识你的用户，主要用于审计，或者用于区分Token颁发给谁
+        String roleSessionName = "MS-"+user.getId();
+        String policy = null;
+        ProtocolType protocolType = ProtocolType.HTTPS;
+
+        AssumeRoleResponse response = OSSFileUpLoadUtils.assumeRole(sonAccessKeyID, sonAccessKeySecret, roleArn,
+                roleSessionName, policy, protocolType);
+        String accesskeyid = response.getCredentials().getAccessKeyId();
+        String accesskeysecret = response.getCredentials().getAccessKeySecret();
+
+        //这个就是我们想要的安全token　　
+        String securitytoken = response.getCredentials().getSecurityToken();
+        System.out.println("accesskeyid==="+accesskeyid);
+        System.out.println("accesskeysecret==="+accesskeysecret);
+        System.out.println("securitytoken==="+securitytoken);
+
+        Map<String, String> respMap = new LinkedHashMap<String, String>();
+        respMap.put("accesskeyid", accesskeyid);
+        respMap.put("accesskeysecret", accesskeysecret);
+        respMap.put("securitytoken", securitytoken);
+        return this.buildSuccessResult(respMap);
+    }
+}
--- src/main/java/com/mshd/util/OSSFileUpLoadUtils.java
+++ src/main/java/com/mshd/util/OSSFileUpLoadUtils.java
@@ -5,6 +5,13 @@ import com.aliyun.oss.model.CannedAccessControlList;
 import com.aliyun.oss.model.GeneratePresignedUrlRequest;
 import com.aliyun.oss.model.OSSObject;
 import com.aliyun.oss.model.ObjectAcl;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import com.aliyuncs.sts.model.v20150401.AssumeRoleRequest;
+import com.aliyuncs.sts.model.v20150401.AssumeRoleResponse;
 import com.mshd.config.Propertyconfig.OSSConfig;
 import org.springframework.context.annotation.Configuration;
 
@@ -137,10 +144,60 @@ public class OSSFileUpLoadUtils {
         return url.toString();
     }
 
+    /**
+     * STS  OSS  获取临时认证
+     * */
+    // 目前只有"cn-hangzhou"这个region可用, 不要使用填写其他region的值
+    public static final String REGION_CN_HANGZHOU = "cn-hangzhou";
+    // 当前 STS API 版本
+    public static final String STS_API_VERSION = "2015-04-01";
+    //静态方法，方便调用
+    public static AssumeRoleResponse assumeRole(String accessKeyId, String accessKeySecret,
+                                                String roleArn, String roleSessionName, String policy,
+                                                ProtocolType protocolType) throws Exception {
+
+        // 创建一个 Aliyun Acs Client, 用于发起 OpenAPI 请求
+        IClientProfile profile = DefaultProfile.getProfile(REGION_CN_HANGZHOU, accessKeyId, accessKeySecret);
+        DefaultAcsClient client = new DefaultAcsClient(profile);
+
+        // 创建一个 AssumeRoleRequest 并设置请求参数
+        final AssumeRoleRequest request = new AssumeRoleRequest();
+        request.setVersion(STS_API_VERSION);
+        request.setMethod(MethodType.POST);
+        request.setProtocol(protocolType);
+
+        request.setRoleArn(roleArn);
+        request.setRoleSessionName(roleSessionName);
+        request.setPolicy(policy);
+
+        // 发起请求，并得到response
+        final AssumeRoleResponse response = client.getAcsResponse(request);
+
+        return response;
+    }
 
     public static void main(String[] args) {
+        //String accessKeyId = "LTAI8hXNVL2gp1Xt";
+        //String accessKeySecret = "Sl5LjsdCaGsemTdHCCtGreacyaUvoe";
+        // 需要在RAM控制台获取，此时要给子账号权限，并建立一个角色，把这个角色赋给子账户，这个角色会有一串值，就是rolearn要填的　　　　　　　　　　
+        // 记得角色的权限，子账户的权限要分配好，不然会报错
+        String accessKeyId = "LTAIHgdiUYGPuRsG";//子账号的accessKeyId
+        String accessKeySecret = "2pgaiHc9TKwcUKwrxt0N2LSy3ZS4qn";//子账号的accessKeySecret;　　　　　　　　　　
+        //临时Token的会话名称，自己指定用于标识你的用户，主要用于审计，或者用于区分Token颁发给谁
+        String roleArn = "acs:ram::1861742825276971:role/aliyunosstokengeneratorrole";
+        String roleSessionName = "MS456";//这个可以为空，不好写，格式要对，无要求建议为空
+        String policy = null;
+        ProtocolType protocolType = ProtocolType.HTTPS;
         try {
-
+            AssumeRoleResponse response = OSSFileUpLoadUtils.assumeRole(accessKeyId, accessKeySecret, roleArn,
+                    roleSessionName, policy, protocolType);
+            String accesskeyid = response.getCredentials().getAccessKeyId();
+            String accesskeysecret = response.getCredentials().getAccessKeySecret();
+            //这个就是我们想要的安全token　　
+            String securitytoken = response.getCredentials().getSecurityToken();
+            System.out.println("accesskeyid==="+accesskeyid);
+            System.out.println("accesskeysecret==="+accesskeysecret);
+            System.out.println("securitytoken==="+securitytoken);
         } catch (Exception e) {
             e.printStackTrace();
         }
