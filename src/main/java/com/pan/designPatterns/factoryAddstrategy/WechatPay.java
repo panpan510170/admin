8a7a6561c01be2e2dfba3047a7766b2b0a314323
1664595314@qq.com <pangaofeng0.0>
2019-06-05 10:10:34 +0800

    学习工厂模式+策略模式  去代替ifElse

---
+++ src/main/java/com/pan/designPatterns/factoryAddstrategy/WechatPay.java
@@ -0,0 +1,15 @@
+package com.pan.designPatterns.factoryAddstrategy;
+
+import java.math.BigDecimal;
+
+/**
+ * 微信支付
+ * @author pan
+ * @date 2019/5/29 15:38
+ */
+public class WechatPay implements PayStrategy {
+    @Override
+    public void pay(BigDecimal total) {
+        System.out.println("pay with wechatpay: " + total);
+    }
+}
---
+++ src/main/java/com/pan/enums/PayTypeEnum.java
@@ -0,0 +1,56 @@
+package com.pan.enums;
+
+/**
+ * 付款类型枚举类
+ * @author pan
+ * @date 2019/5/29 14:58
+ */
+public enum PayTypeEnum {
+    aliPay(1,"支付宝支付","com.pan.designPatterns.factoryAddstrategy.AliPay"),
+    wechatPay(2,"微信支付","com.pan.designPatterns.factoryAddstrategy.WechatPay"),
+    lianlianPay(3,"连连支付",""),
+    rongbaoPay(4,"融宝支付","");
+
+    private int id;
+    private String name;
+    private String className;
+
+    PayTypeEnum(int id, String name,String className) {
+        this.id = id;
+        this.name = name;
+        this.className = className;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public static PayTypeEnum getPayTypeEnumByName(int id) {
+        for (PayTypeEnum t : PayTypeEnum.values()) {
+            if (t.getId() == id) {
+                return t;
+            }
+        }
+        return null;
+    }
+}
